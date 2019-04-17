24cb92b8b7dd6ff8119c5a1b61e69df3fce8cead
marcelojrfarias <marcelojrfarias@outlook.com>
2019-04-17 11:56:33 -0300

    Adiciona a conexão com o ThingSpeak

--- cai3d.ino
+++ cai3d.ino
@@ -1,9 +1,28 @@
 // Config file
 #include "config.h"
 
+// Credentials file
+#include "mycredentials.h"
+
+// ThingSpeak library and parameters
+#include "ThingSpeak.h"
+unsigned long myChannelNumber = CREDENTIALS_CH_ID;
+const char * myWriteAPIKey = CREDENTIALS_WRITE_APIKEY;
+
+// WiFi library and parameters
+#include <WiFi.h>
+char ssid[] = CREDENTIALS_SSID;   // your network SSID (name) 
+char pass[] = CREDENTIALS_PASS;   // your network password
+int keyIndex = 0;            // your network key Index number (needed only for WEP)
+WiFiClient  client;
+
 // PCF8591 8-bit A/D and D/A converter
 #include "PCF8591.h"
 PCF8591 pcf8591(PCF8591_I2C_ADDRESS);
+
+// Envoriment data
+int insideTemperature = 0;
+float insideLuminosity = 0;
 
 void setup() {
   // Start the logging serial port
@@ -15,12 +34,64 @@ void setup() {
   // Start the AD/DA converter
   pcf8591.begin();
   setFanSpeed(100);
+
+  // Initialize the connection
+  WiFi.mode(WIFI_STA);   
+
+  // Initialize ThingSpeak
+  ThingSpeak.begin(client);  
 }
 
 void loop() {
-	LOG.print(getInsideTemperature()); LOG.print("\t");
-	LOG.print(getInsideLuminosity()); LOG.print("\n");
+  // Connect to WiFi network
+  wifiConnect();
+
+  // Update the sensors values
+  insideTemperature = getInsideTemperature();
+	LOG.print(insideTemperature); LOG.print("\t");
+  insideLuminosity = getInsideLuminosity();
+	LOG.print(insideLuminosity); LOG.print("\t");
   delay(500);
+
+  // Update the ThingSpeak channels
+  updateThingSpeakChannel();
+
+  // write to the ThingSpeak channel - send to internet
+  int statusCode = ThingSpeak.writeFields(myChannelNumber, myWriteAPIKey);
+
+  // Verifies the HTTP status code
+  if(statusCode == 200){ // successful
+    LOG.println("Channel update successful.");
+  }
+  else{ // Error
+    LOG.println("Problem updating channel. HTTP error code " + String(statusCode));
+  }
+
+  // Wait 20 seconds for the next update
+  delay(20000);
+}
+
+// Connection manager
+void wifiConnect() {
+  // Connect or reconnect to WiFi
+  if(WiFi.status() != WL_CONNECTED){
+    LOG.print("Attempting to connect to SSID: ");
+    LOG.println(CREDENTIALS_SSID);
+    while(WiFi.status() != WL_CONNECTED){
+      // Connect to WPA/WPA2 network. Change this line if using open or WEP network
+      WiFi.begin(ssid, pass);  
+      LOG.print(".");
+      delay(5000);     
+    } 
+    LOG.println("Connected.\n");
+  }
+}
+
+// Update the fields with the sensor values
+void updateThingSpeakChannel() {
+  ThingSpeak.setStatus("CAI3D");
+  ThingSpeak.setField(1, insideTemperature);
+  ThingSpeak.setField(2, insideLuminosity);
 }
 
 // Reads the temperature read by the LM35 through the PCF8591 in degrees Celsius
@@ -29,8 +100,8 @@ int getInsideTemperature() {
 }
 
 // Reads the LDR luminosity through the PCF8591
-int getInsideLuminosity() {
-  return pcf8591.voltageRead(PCF8591_SENSOR_LIGHT) * PCF8591_FACTOR_LIGHT;
+float getInsideLuminosity() {
+  return 100.0 - pcf8591.voltageRead(PCF8591_SENSOR_LIGHT) * PCF8591_FACTOR_LIGHT;
 }
 
 // Set the fan speed through the AD/DA converter and convert output
---
+++ credentials.h
@@ -0,0 +1,13 @@
+
+#ifndef CREDENTIALS_h
+#define CREDENTIALS_h
+
+// Connection credentials
+#define CREDENTIALS_SSID "MySSID"		// replace MySSID with your WiFi network name
+#define CREDENTIALS_PASS "MyPassword"	// replace MyPassword with your WiFi password
+
+// ThingSpeak credentials
+#define CREDENTIALS_CH_ID 0000000			// replace 0000000 with your channel number
+#define CREDENTIALS_WRITE_APIKEY "XYZ"   // replace XYZ with your channel write API Key
+
+#endif
